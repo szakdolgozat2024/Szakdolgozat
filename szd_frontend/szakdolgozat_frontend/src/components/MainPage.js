649221142cedf20aa256775438e6ff4d7bdc7d46
KSZBence <kunszekelybence@gmail.com>
2024-02-13 15:41:11 +0100

    css javitás

--- szd_frontend/szakdolgozat_frontend/src/components/MainPage.js
+++ szd_frontend/szakdolgozat_frontend/src/components/MainPage.js
@@ -7,7 +7,7 @@ export default function MainPage(props) {
     return (
         <main className="text-center">
             <Navbar></Navbar>
-            <div className="container mx-auto mt-20 p-80">
+            <div className="mainContent container mx-auto mt-20 p-80">
                 <div className="row w-100">
                     <SideMenu btsCol="col-2"></SideMenu>
                     <ProductContainer btsCol="col-9"></ProductContainer>
